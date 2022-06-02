16b559d15b97777126c30d8c63a9990453cbfac5
Laise France <laise94@hotmail.com>
2022-06-02 07:16:21 -0300

    add evento ao botao

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.scss']
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
 
   transferir() {
-    console.log('solicitado nova-transferencia')
+    console.log('Solicitada nova transferência');
   }
-
-
 }
