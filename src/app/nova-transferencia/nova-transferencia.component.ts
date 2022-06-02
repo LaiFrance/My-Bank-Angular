449169b9db5906c7fa728fed2fe190b54b8e6ca4
Laise France <laise94@hotmail.com>
2022-06-02 20:15:33 -0300

    add valor e destino exibindo valores na tela

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -7,7 +7,16 @@ import { Component } from '@angular/core';
 })
 export class NovaTransferenciaComponent {
 
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor!: number;
+  destino!: number;
+
   transferir() {
     console.log('Solicitada nova transferência');
+    const valorEmitir = {valor: this.valor, destino: this.destino}
+    this.aoTransferir.emit(valorEmitir);
+    // console.log('Valor: ', this.valor);
+    // console.log('Destino: ', this.destino);
   }
 }
